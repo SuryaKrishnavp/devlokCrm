71707a47b32f164dcd5fc53825b6f5ca63ea85bb
Suryakrishnavp <suryakrishnavaliyapurakkal@gmail.com>
2025-04-17 09:27:12 +0530

    Updated

--- devlok_crm/asgi.py
+++ devlok_crm/asgi.py
@@ -6,7 +6,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 import leads_section.routing
-# import followup_section.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devlok_crm.settings")
 django.setup()
@@ -17,7 +16,6 @@ application = ProtocolTypeRouter(
         "websocket": AuthMiddlewareStack(  
             URLRouter(
                 leads_section.routing.websocket_urlpatterns 
-                # followup_section.routing.get_websocket_urlpatterns() 
             )
         ),
     }
