03d4a95f83d90935bfc4ee2ab973033f9e4011f4
Suryakrishnavp <suryakrishnavaliyapurakkal@gmail.com>
2025-04-22 14:08:26 +0530

    Matching pdf created

--- devlok_crm/settings.py
+++ devlok_crm/settings.py
@@ -127,7 +127,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
 
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
